c1dc5d70056ab686ac12307a7a1b7ee4b14d6a50
nettefee <nettefee@googlemail.com>
2026-07-19 16:56:53 +0200

    Add files via upload

--- app/src/main/java/de/psygrenz/app/ReaderActivity.java
+++ app/src/main/java/de/psygrenz/app/ReaderActivity.java
@@ -24,6 +24,7 @@ import java.util.Locale;
 import java.util.TreeSet;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.UUID;
 
 public class ReaderActivity extends Activity {
     private TextView body;
